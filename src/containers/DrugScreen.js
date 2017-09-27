da259f3a54e0eafc05ea612cac0f7b39977ed515
Juman Pak <qkrwnaks92@gmail.com>
2017-09-28 04:47:25 +0900

    DrugScreen Modal ing

--- src/containers/DrugScreen.js
+++ src/containers/DrugScreen.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, View, Text, Image, ScrollView, Modal, TouchableHighlight } from 'react-native';
 
 class DrugScreen extends Component {
     static navigationOptions = {
         headerTitle: '투약 정보'
     }
 
+    state = {
+        modalVisible: false,
+    }
+
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
+
     renderDrugDetail = (drugDetail) => {
         console.log(drugDetail);
         return drugDetail.map((data, index) => {
             return (
-                <View key={index} style={styles.drugDetailStyle}>
-                    <Image source={require('../imgs/A11A4380A004901.jpg')} style={styles.drugImageStyle} />
-                    <Text style={styles.drugDescription}>{data.drugName}</Text>
+                <View style={styles.drugDetailStyle}>
+                    <Modal
+                        animationType="fade"
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {alert("Modal has been closed.")}}
+                        presentationStyle={'fullScreen'}
+                    >
+                        <View style={styles.modalContainerStyle}>
+                            
+        
+                            <TouchableHighlight onPress={() => {
+                                this.setModalVisible(!this.state.modalVisible)
+                            }}>
+                            <View>
+                                <Image resizeMode='stretch' source={require('../imgs/A11A4380A004901.jpg')} style={styles.modalDrugImageStyle} />
+                                <Text style={styles.modalDrugNameStyle}>약 이름</Text>
+                                <Text style={styles.modalDrugDetailStyle}>약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. 약 상세정보입니다. </Text>
+                            </View>
+                            </TouchableHighlight>
+        
+                        </View>
+                    </Modal>
+      
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <View key={index} >
+                            <Image source={require('../imgs/A11A4380A004901.jpg')} style={styles.drugImageStyle} />
+                            <Text style={styles.drugDescription}>{data.drugName}</Text>
+                        </View>
+                    </TouchableHighlight>
                 </View>
+
+                
             );
         });
     }
@@ -104,6 +142,31 @@ const styles = StyleSheet.create({
                 },
                 drugDescription: {
                     textAlign: 'center'
+                },
+            modalContainerStyle: {
+                borderRadius: 10,
+                shadowOffset: {
+                    width: 0,
+                    height: 0
+                },
+                shadowColor: 'grey',
+                shadowOpacity: 0.3,
+                padding: 10,
+                marginTop: 50,
+                margin: 20
+            },
+                modalDrugImageStyle: {
+                    width: '100%',
+                    height: 140,
+                    marginBottom: 10
+                },
+                modalDrugNameStyle: {
+                    textAlign: 'center',
+                    fontSize: 20,
+                    fontWeight: 'bold'
+                },
+                modalDrugDetailStyle: {
+                    margin: 5
                 }
 })
 
