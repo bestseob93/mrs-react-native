5d2d6dac890d3d66d353cf72c20536503b41b3b1
Juman Pak <qkrwnaks92@gmail.com>
2017-09-28 04:48:58 +0900

    InfoScreen Profile complete

--- src/containers/InfoScreen.js
+++ src/containers/InfoScreen.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import Dimensions from 'Dimensions';
-import { FlatList, StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, View, Text, Image, ScrollView, AsyncStorage, Button } from 'react-native';
 
 class InfoScreen extends Component {
     static navigationOptions = {
         headerTitle: '한성병원'
+    }
+
+    onLogoutPress = async () => {
+        console.log('pressed');
+        const { navigation } = this.props;
+        try {
+            await AsyncStorage.removeItem('myToken');
+            navigation.navigate('SplashScreen');
+        } catch (e) {
+            if(e) throw e;
+        }
     }
 
     render() {
@@ -31,23 +42,38 @@ class InfoScreen extends Component {
 
 
 
-        const { renderHospInfo, renderDoctorList } = this;
+        const { renderHospInfo, renderDoctorList, onLogoutPress } = this;
         return (
             <View style={styles.infoContainer}>
                 <View style={styles.imageContainerStyle}>
-                    <Image source={require('../imgs/main_image.jpeg')} style={styles.hospImageStyle} />
-                </View>
-                <View style={styles.doctorListStyle}>
-                    <View style={ [styles.profileStyle, {backgroundColor:'yellow'}] }>
-                        <Text>이환섭</Text>
-                    </View>
-                    <View style={ [styles.profileStyle, {backgroundColor:'yellow'}] }>
-                        <Text>박주만</Text>
+                    <Image resizeMode='cover' source={require('../imgs/main_image.jpeg')} style={styles.hospImageStyle} />
+                    <View style={styles.hospCommentStyle}>
+                        <Text style={{color:'white'}}>한성병원은 여러분을 응원합니다.</Text>
                     </View>
                 </View>
                 <View style={styles.doctorListStyle}>
-                    <View style={ [styles.profileStyle, {backgroundColor:'yellow'}] }>
-                        <Text>권형은</Text>
+                    <View style={ [styles.profileContainerStyle] }>
+                        <Image resizeMode='cover' source={require('../imgs/lee.jpeg')} style={styles.profileImageStyle} />
+                        <View style={styles.profileCommentStyle}>
+                            <Text style={{color:'white'}}>병원장                   이환섭</Text>
+                        </View>
+                    </View>
+                    <View style={ [styles.profileContainerStyle] }>
+                        <Image resizeMode='cover' source={require('../imgs/kang.jpeg')} style={styles.profileImageStyle} />
+                        <View style={styles.profileCommentStyle}>
+                            <Text style={{color:'white'}}>부원장                   강민호</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.doctorListStyle}>
+                    <View style={ [styles.profileContainerStyle] }>
+                        <Image resizeMode='cover' source={require('../imgs/kwon.jpeg')} style={styles.profileImageStyle} />
+                        <View style={styles.profileCommentStyle}>
+                            <Text style={{color:'white'}}>담당의사                권형은</Text>
+                        </View>
+                    </View>
+                    <View style={ [styles.logoutButtonStyle] }>
+                        <Button title='로그아웃' onPress={onLogoutPress} />
                     </View>
                 </View>
             </View>
@@ -61,29 +87,58 @@ const styles = StyleSheet.create({
         flex: 1
     },
         imageContainerStyle: {
-            flex: 0.3,
-            shadowOffset: {
-                width: 0,
-                height: 2
-            },
-            shadowColor: 'black',
-            shadowOpacity: 1,
-            marginBottom: 20
+            // shadowOffset: {
+            //     width: 0,
+            //     height: 1
+            // },
+            // shadowColor: 'black',
+            // shadowOpacity: 1,
+            flex: 0.35,
+            position: 'relative'
         },
             hospImageStyle: {
-                width: 375,
-                height: 200
+                flex: 1,
+                width: null,
+                height: null
             },
-        doctorListStyle: {
-            backgroundColor: 'skyblue',
-            flexDirection: 'row',
-            flex: 0.7,
-        },
-            profileStyle: {
-                flex: 0.5,
-            }
-            
+            hospCommentStyle: {
+                backgroundColor: 'transparent',
+                position: 'absolute',
+                right: 10,
+                bottom: 15
+            },
 
+        doctorListStyle: {
+            flex: 0.31,
+            flexDirection: 'row'
+        },
+            profileContainerStyle: {
+                flex: 0.48,
+                // justifyContent: 'center',
+                // alignItems: 'center',
+                marginLeft: 15,
+                marginTop: 15,
+                position: 'relative'
+            },
+                profileImageStyle: {
+                    flex: 1,
+                    width: null,
+                    height: null
+                },
+                profileCommentStyle: {
+                    backgroundColor: 'transparent',
+                    position: 'absolute',
+                    left: 10,
+                    bottom: 5
+                },
+        
+            logoutButtonStyle: {
+                flex: 0.48,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginLeft: 15,
+                marginTop: 15,
+            }
 })
 
 export default InfoScreen;
